d27df5dcf0f2a417be479420f5c5892b003404b2
tgao1337 <gaotommy10@gmail.com>
2018-07-16 15:44:35 -0700

    fixed task
    fixed type spelling and added default to the completed

--- models/task.js
+++ models/task.js
@@ -3,8 +3,8 @@ const Schema = mongoose.Schema;
 
 const TaskSchema = new Schema({
   task: { type: String, required: true },
-  dueDate: { tpye: Date,required: true },
-  completed: { tpye: Boolean, required: true},
+  dueDate: { type: Date,required: true },
+  completed: { type: Boolean, required: true, default: 'false'},
 });
 
 module.exports = mongoose.model('Task', TaskSchema);
