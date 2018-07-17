0935cd76a65e6a1bc57d87a144ebd062b807769e
tgao1337 <gaotommy10@gmail.com>
2018-07-17 15:57:10 -0700

    Fixed the merge conflicts in model and viewtask

--- models/task.js
+++ models/task.js
@@ -3,19 +3,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const TaskSchema = new Schema({
- task: { type: String, required: true },
-estimatedTime: { type: Number, required: false },
-
-dueDate: { type: Date, required: false },
-
-objc: { type: String, required: false },
-
-badc: { type: String, required: false },
-
-rewar: { type: String, required: false },
-
-completed: { type: Boolean, required: false, default: 'false' },
-
+  task: { type: String, required: true },
+  estimatedTime: { type: Number, required: false },
+  dueDate: { type: Date, required: false },
+  objc: { type: String, required: false },
+  badc: { type: String, required: false },
+  rewar: { type: String, required: false },
+  completed: { type: Boolean, required: false, default: 'false' },
 });
 
 module.exports = mongoose.model('Task', TaskSchema);
