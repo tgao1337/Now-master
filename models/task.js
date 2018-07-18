5ecfc9da0f4e2fcff5f357bd1f1e232b3d2e4699
Yi Zu <zuyi@bu.edu>
2018-07-18 15:48:35 -0700

    Backup for Date
    Need to figure out why ddm, ddd, ddy (month, day, yeaer) is not showing up on index.hbs.
    
    MongoDB problem?

--- models/task.js
+++ models/task.js
@@ -5,7 +5,10 @@ const Schema = mongoose.Schema;
 const TaskSchema = new Schema({
   task: { type: String, required: true },
   estimatedTime: { type: Number, required: false },
-  dueDate: { type: Date, required: false },
+  // dueDate: { type: Date, required: false },
+  ddm : { type: Number, required: true },
+  ddd : { type: Number, required: true },
+  ddy : { type: Number, required: true },
   objc: { type: String, required: false },
   badc: { type: String, required: false },
   rewar: { type: String, required: false },
